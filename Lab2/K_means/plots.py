7c94ff5b725ea5df70f0908995166998b5f02e96
MEGlr <94239704+MEGlr@users.noreply.github.com>
2022-11-11 15:52:54 +0200

    corrected plots

--- Lab2/K_means/plots.py
+++ Lab2/K_means/plots.py
@@ -102,14 +102,18 @@ def plot_time_speedup(title_, input_loc1, input_loc2, output, legend1, legend2 )
         plt.savefig(output+"_time.png", bbox_inches="tight")
 
 
-plot_time_speedup("k-means (naive parallel)","kmeans_shared_affinity/run_kmeans.out", "kmeans_shared/run_kmeans.out" ,"plots/k_means_naive.png", "affinity", "no affinity" )
+plot_time_speedup("k-means (naive parallel)","outputs/run_kmeans_naive_with_aff.out", "outputs/run_kmeans_naive.out" ,"plots/k_means_naive", "affinity", "no affinity" )
 
-plot_time_speedup("k-means - reduction  (i)","kmeans_reduction_i_without_parallelism/run_kmeans_reduction.out", "" ,"plots/k_means_reduction_i.png", "", "" )
+plot_time_speedup("k-means - reduction  (i)","outputs/run_kmeans_reduction_i_16.out", "" ,"plots/k_means_reduction_i", "", "" )
 
-plot_time_speedup("k-means - reduction  (ii) \n Coords = 1, Clusters = 4","kmeans_reduction_ii_init_parallel_inside_while_1_4/run_kmeans_reduction.out", "" ,"plots/k_means_reduction_ii_1_4.png", "", "" )
+plot_time_speedup("k-means - reduction  (ii) \n Coords = 1, Clusters = 4","outputs/run_kmeans_reduction_ii_4.out", "" ,"plots/k_means_reduction_ii_1_4", "", "" )
 
-plot_time_speedup("k-means - reduction  (ii) \n Coords = 1, Clusters = 4 - calloc","kmeans_reduction_ii_parallel_calloc_1_4/run_kmeans_reduction.out", "" ,"plots/k_means_reduction_ii_calloc_1_4.png", "", "" )
+plot_time_speedup("k-means - reduction  (ii) \n Coords = 1, Clusters = 4 - calloc","outputs/run_kmeans_reduction_ii_parallel_4.out", "" ,"plots/k_means_reduction_ii_calloc_1_4", "", "" )
 
-plot_time_speedup("k-means - reduction  (ii) \n Coords = 16, Clusters = 16","kmeans_reduction_ii_init_parallel_inside_while_16_16/run_kmeans_reduction.out", "" ,"plots/k_means_reduction_ii_16_16.png", "", "" )
+#plot_time_speedup("k-means - reduction  (ii) \n Coords = 16, Clusters = 16","outputs/run_reduction_ii_16.out", "" ,"plots/k_means_reduction_ii_16_16", "", "" )
 
-plot_time_speedup("k-means - reduction  (ii) \n Coords = 16, Clusters = 16 - calloc","kmeans_reduction_ii_parallel_calloc_16_16/run_kmeans_reduction.out", "" ,"plots/k_means_reduction_ii_calloc_16_16.png", "", "" )
+plot_time_speedup("k-means - reduction  (ii) \n Coords = 16, Clusters = 16 - calloc","outputs/run_kmeans_reduction_ii_16.out", "" ,"plots/k_means_reduction_ii_calloc_16_16", "", "" )
+
+plot_time_speedup("k-means - reduction  (bonus) \n Coords = 16, Clusters = 16","outputs/run_kmeans_bonus_16.out", "" ,"plots/k_means_reduction_bonus16", "", "" )
+
+plot_time_speedup("k-means - reduction  (bonus) \n Coords = 1, Clusters = 4","outputs/run_kmeans_bonus_4.out", "" ,"plots/k_means_reduction_bonus4", "", "" )
