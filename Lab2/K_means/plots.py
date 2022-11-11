cf5709036ee8f976864cc4854eda0d536dc5e241
MEGlr <94239704+MEGlr@users.noreply.github.com>
2022-11-11 16:28:53 +0200

    updated report with new plots still wrong

--- Lab2/K_means/plots.py
+++ Lab2/K_means/plots.py
@@ -102,18 +102,25 @@ def plot_time_speedup(title_, input_loc1, input_loc2, output, legend1, legend2 )
         plt.savefig(output+"_time.png", bbox_inches="tight")
 
 
+#seq_time16 = seq_time
+
+
+
+
 plot_time_speedup("k-means (naive parallel)","outputs/run_kmeans_naive_with_aff.out", "outputs/run_kmeans_naive.out" ,"plots/k_means_naive", "affinity", "no affinity" )
 
 plot_time_speedup("k-means - reduction  (i)","outputs/run_kmeans_reduction_i_16.out", "" ,"plots/k_means_reduction_i", "", "" )
 
+plot_time_speedup("k-means - reduction  (ii) \n Coords = 16, Clusters = 16 - calloc","outputs/run_kmeans_reduction_ii_16.out", "" ,"plots/k_means_reduction_ii_calloc_16_16", "", "" )
+
+plot_time_speedup("k-means - reduction  (bonus) \n Coords = 16, Clusters = 16","outputs/run_kmeans_bonus_16.out", "" ,"plots/k_means_reduction_bonus16", "", "" )
+
+seq_time = 11.5035
 plot_time_speedup("k-means - reduction  (ii) \n Coords = 1, Clusters = 4","outputs/run_kmeans_reduction_ii_4.out", "" ,"plots/k_means_reduction_ii_1_4", "", "" )
 
 plot_time_speedup("k-means - reduction  (ii) \n Coords = 1, Clusters = 4 - calloc","outputs/run_kmeans_reduction_ii_parallel_4.out", "" ,"plots/k_means_reduction_ii_calloc_1_4", "", "" )
 
 #plot_time_speedup("k-means - reduction  (ii) \n Coords = 16, Clusters = 16","outputs/run_reduction_ii_16.out", "" ,"plots/k_means_reduction_ii_16_16", "", "" )
 
-plot_time_speedup("k-means - reduction  (ii) \n Coords = 16, Clusters = 16 - calloc","outputs/run_kmeans_reduction_ii_16.out", "" ,"plots/k_means_reduction_ii_calloc_16_16", "", "" )
-
-plot_time_speedup("k-means - reduction  (bonus) \n Coords = 16, Clusters = 16","outputs/run_kmeans_bonus_16.out", "" ,"plots/k_means_reduction_bonus16", "", "" )
 
 plot_time_speedup("k-means - reduction  (bonus) \n Coords = 1, Clusters = 4","outputs/run_kmeans_bonus_4.out", "" ,"plots/k_means_reduction_bonus4", "", "" )
