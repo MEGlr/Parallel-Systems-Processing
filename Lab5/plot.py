cac200837f72969586ba3d3b793515c81c5fe63f
meglr <94239704+MEGlr@users.noreply.github.com>
2023-01-08 01:39:01 +0200

    new plots

--- Lab5/plot.py
+++ Lab5/plot.py
@@ -3,18 +3,20 @@ import sys
 import numpy as np
 import matplotlib
 #matplotlib.use('Agg')
-
+color = ["#588c7e", "#96ceb4" ,"#b5e7a0"]
 block_size = ["32", "64", "128", "256", "512", "1024"]
 versions = ["Naive version", "Transpose version", "Shared version"]
-def plot_time_speedup(input_file):
+def plot_time_speedup(input_file, max_):
     fp = open(str(input_file))
     data_time = {}
     data_speedup = {}
     fp.readline()  #ignore headers
     line = fp.readline()   
     seq_time = float(line.split(",")[2])  #TODO
-
-    for version in versions:
+    coo_ = input_file.split("Coo-")[1]
+    coo_ = coo_.split("_Cl")[0]
+    fig, ax = plt.subplots(2, 1,figsize=(10, 8))
+    for i, version in enumerate(versions[:max_]):
         index = 0
         while index < len(block_size):
             size = block_size[index]
@@ -30,34 +32,40 @@ def plot_time_speedup(input_file):
         values_time = list(data_time.values())
         values_speedup = list(data_speedup.values())
         print("values", values_time)
-        coo_ = input_file.split("Coo-")[1]
-        coo_ = coo_.split("_Cl")[0]
-        fig = plt.figure(figsize = (10, 5))
-        plt.title("K-means - " + version + " (time plot)"  + f", coordinates = {coo_}")
-        bar_time = plt.bar(np.arange(len(block_size)+1), [seq_time] + values_time, color ='green',
-            width =0.2)
+        offset = (i-int(max_/2)/2+int(max_/3)/2-int((max_-1)/2))*0.2
+        ax[0].set_title("K-means - "  + f"coordinates = {coo_} (time plot)")
+        bar_time = ax[0].bar(np.arange(len(block_size)+1)+offset, [seq_time] + values_time, color =color[i],
+            width =0.2, label = version)
 
-        plt.xlabel("block size")
-        plt.ylabel("time (in sec)")
-        plt.xticks(np.arange(len(block_size)+1) , ["sequential"]+block_size)
-        
-        plt.savefig(version+"_time"+f"_coo_{coo_}", bbox_inches="tight")
 
-        fig = plt.figure(figsize = (10, 5))
         
-        plt.title("K-means - " + version + " (speedup plot) " + f", coordinates = {coo_}")
-        bar_time = plt.bar(np.arange(len(block_size)+1), [seq_time] + values_speedup, color ='green',
-            width = 0.2)
-        plt.xlabel("block size")
-        plt.ylabel("speedup")
-        plt.xticks(np.arange(len(block_size)+1) , ["sequential"]+block_size)
-        plt.savefig(version+"_speedup"+f"_coo_{coo_}", bbox_inches="tight")
+        ax[1].set_title("K-means - "  +f"coordinates = {coo_} (speedup plot)")
+        bar_time = ax[1].bar(np.arange(len(block_size)+1)+offset, [1] + values_speedup, color =color[i],
+            width = 0.2, label = version)
+    
+    ax[0].set_xlabel("block size")
+    ax[0].set_ylabel("time (in sec)")
+    ax[0].set_xticks(np.arange(len(block_size)+1) , ["sequential"]+block_size)
+    
+
+    ax[1].set_xlabel("block size")
+    ax[1].set_ylabel("speedup")
+    ax[1].set_xticks(np.arange(len(block_size)+1) , ["sequential"]+block_size)
+    ax[0].legend()
+    ax[1].legend()
+    plt.tight_layout()
+    plt.savefig("up_to_"+version+"_speedup"+f"_coo_{coo_}", bbox_inches="tight")
+    plt.savefig("up_to_"+version+"_time"+f"_coo_{coo_}", bbox_inches="tight")
 
     fp.close() 
         
-plot_time_speedup("Sz-256_Coo-2_Cl-16_with_val.csv")
+plot_time_speedup("Sz-256_Coo-2_Cl-16_with_val.csv", 1)
+plot_time_speedup("Sz-256_Coo-2_Cl-16_with_val.csv", 2)
+plot_time_speedup("Sz-256_Coo-2_Cl-16_with_val.csv", 3)
 
-plot_time_speedup("Sz-256_Coo-16_Cl-16.csv")
+# plot_time_speedup("Sz-256_Coo-16_Cl-16.csv", 1)
+# plot_time_speedup("Sz-256_Coo-16_Cl-16.csv", 2)
+plot_time_speedup("Sz-256_Coo-16_Cl-16.csv", 3)
 
 
 # temp = input_file.split("Sz-")[1]
